7b09bf1732d7e1c2d4d490da28b22900373c32e0
Javi <janais_9@hotmail.com>
2026-05-14 16:57:07 -0400

    correcciones

--- MicroServicioSucursal/src/main/java/com/comercio/sucursal/SucursalApplication.java
+++ MicroServicioSucursal/src/main/java/com/comercio/sucursal/SucursalApplication.java
@@ -2,8 +2,12 @@ package com.comercio.sucursal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.comercio.sucursal.repository")
+@EntityScan(basePackages = "com.comercio.sucursal.model")
 public class SucursalApplication{
 
     public static void main(String[] args){
--- MicroServicioSucursal/src/main/java/com/comercio/sucursal/repository/SucursalRepository.java
+++ MicroServicioSucursal/src/main/java/com/comercio/sucursal/repository/SucursalRepository.java
@@ -6,7 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
-
-
-
 }
