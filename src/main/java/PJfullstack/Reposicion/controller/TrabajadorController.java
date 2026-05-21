7b6eb746a688916f4f286176fec582087cf9adbb
Pavel <pavelolmedoo@gmail.com>
2026-05-21 00:07:40 -0400

    Sistema CRUD hecho + ResponseEntity

--- src/main/java/PJfullstack/Reposicion/controller/TrabajadorController.java
+++ src/main/java/PJfullstack/Reposicion/controller/TrabajadorController.java
@@ -3,18 +3,42 @@ package PJfullstack.Reposicion.controller;
 import PJfullstack.Reposicion.entity.Trabajador;
 import PJfullstack.Reposicion.service.TrabajadorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/trabajadores")
 
 public class TrabajadorController {
    @Autowired
     private TrabajadorService trabajadorService;
+   // Crear un trabajador
    @PostMapping
-   public Trabajador crearTrabajador(@RequestBody Trabajador nuevoTrabajador){
-       return trabajadorService.guardarTrabajador(nuevoTrabajador);
+   public ResponseEntity<Trabajador> crearTrabajador(@RequestBody Trabajador nuevoTrabajador){
+       return ResponseEntity.status(HttpStatus.CREATED).body(trabajadorService.guardarTrabajador(nuevoTrabajador));
    }
+   // Listar TODOS los trabajadores registrados
+   @GetMapping
+   public ResponseEntity<List<Trabajador>> listarTrabajador(){
+       return ResponseEntity.ok(trabajadorService.listarTodosTrabajadores());
+   }
+   // Listar un trabajador en especifico
+    @GetMapping("/{id}")
+    public ResponseEntity<Trabajador> listarUnTrabajador(@PathVariable Long id){
+       return ResponseEntity.ok(trabajadorService.listarUnSoloTrabajador(id));
+    }
+   // Actualizar trabajador
+    @PutMapping("/{id}")
+    public ResponseEntity<Trabajador> actualizarTrabajadores(@PathVariable Long id,@RequestBody Trabajador trabajadorActualizado ){
+       Trabajador trabajadorGuardado = trabajadorService.actualizarTrabajador(id,trabajadorActualizado );
+    return ResponseEntity.ok(trabajadorGuardado);
+    }
+    // Eliminar trabajador
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarTrabajador(@PathVariable Long id){
+    trabajadorService.eliminarTrabajador(id);
+    return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/PJfullstack/Reposicion/service/TrabajadorService.java
+++ src/main/java/PJfullstack/Reposicion/service/TrabajadorService.java
@@ -5,13 +5,40 @@ import PJfullstack.Reposicion.repository.TrabajadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TrabajadorService {
 
     @Autowired
     private TrabajadorRepository trabajadorRepository;
 
+    // Crear un trabajador
     public Trabajador guardarTrabajador(Trabajador nuevoTrabajador){
         return trabajadorRepository.save(nuevoTrabajador);
+    }
+    // Listar TODOS los trabajadores registrados
+    public  List<Trabajador> listarTodosTrabajadores(){
+        return trabajadorRepository.findAll();
+    }
+    // Listar un trabajador en especifico
+    public Trabajador listarUnSoloTrabajador(Long id){
+        Trabajador trabajadorExistente = trabajadorRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado" + id));
+        return trabajadorExistente;
+    }
+    // Actualizar trabajador
+    public Trabajador actualizarTrabajador(Long id, Trabajador trabajadorActualizado){
+        Trabajador trabajadorExistente = trabajadorRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado" + id));
+        trabajadorExistente.setRut(trabajadorActualizado.getRut());
+        trabajadorExistente.setNombre(trabajadorActualizado.getNombre());
+        trabajadorExistente.setCorreo(trabajadorActualizado.getCorreo());
+        trabajadorExistente.setRol(trabajadorActualizado.getRol());
+        trabajadorExistente.setEdad(trabajadorActualizado.getEdad());
+        return trabajadorRepository.save(trabajadorExistente);
+    }
+    //Eliminar trabajador
+    public void eliminarTrabajador(Long id){
+        Trabajador trabajadorExistente = trabajadorRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado" + id));
+        trabajadorRepository.deleteById(id);
     }
 }
