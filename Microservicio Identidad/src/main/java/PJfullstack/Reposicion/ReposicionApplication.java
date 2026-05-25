9ebdae5bcebf8e2a7711418d0d5f1f90139f6613
Javi <janais_9@hotmail.com>
2026-05-24 21:01:18 -0400

    correcciones

--- Microservicio Identidad/src/main/java/PJfullstack/Reposicion/ReposicionApplication.java
+++ Microservicio Identidad/src/main/java/PJfullstack/Reposicion/ReposicionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReposicionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReposicionApplication.class, args);
 	}
