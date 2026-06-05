1fe5425d6066670ef0a172b3928221653bfc2add
Pavel <pavelolmedoo@gmail.com>
2026-06-05 19:12:35 -0400

    chore: estandarización de propiedades, dependencias y configuración global
    - Corrección de dependencias en todos los microservicios.
    - Agregar comentarios a las dependencias en todos los microservicios.
    - Agregar dependencia swagger en todos los microservicios.
    - Agregar propiedades swagger en todos los microservicios.
    - Creación de carpeta config en todos los microservicios (completando los faltantes).
    - Creación de clases SwaggerConfig en todos los microservicios.
    - Creación de anotación @JsonFormat en el TurnoDTO.
    - Modificación de propiedades para agregar la propiedad de identificación en el microservicio Sucursal y Ventas.
    - Eliminación de permiso para sobrescribir Bean en propiedades del microservicio Inventario.
    - Arreglar microservicio IDENTIDAD: corrección de nombre identificatorio erróneo ("Reposicion") en las propiedades.

--- Microservicio Identidad/src/main/java/PJfullstack/Reposicion/dto/TurnoDTO.java
+++ Microservicio Identidad/src/main/java/PJfullstack/Reposicion/dto/TurnoDTO.java
@@ -1,5 +1,6 @@
 package PJfullstack.Reposicion.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -11,9 +12,11 @@ public class TurnoDTO {
     @NotBlank(message = "El tipo de turno no puede estar vacio")
     private String tipoTurno;
 
+    @JsonFormat(pattern = "HH:mm:ss")
     @NotNull(message = "La hora de inicio es obligatoria")
     private LocalTime horaInicio;
 
+    @JsonFormat(pattern = "HH:mm:ss")
     @NotNull(message = "La hora de termino es obligatoria")
     private LocalTime horaTermino;
 }
